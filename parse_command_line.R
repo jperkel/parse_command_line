19b419398e03bc51d960c9ce90c145b9910e5f65
Jeffrey Perkel <j_perkel@yahoo.com>
2019-09-15 16:59:10 -0600

    Remove support for no lparam.

--- parse_command_line.R
+++ parse_command_line.R
@@ -105,7 +105,7 @@ usage <- function() {
   writeLines('  PARAMETERS:')
   writeLines(paste0('\t',str_pad(args_table$lparam, max(nchar(args_table$lparam)), "right"),
                     # need 5 spaces to account for missing sparam, eg ' (-m)'
-                   ifelse (args_table$sparam != '', paste0(' (', args_table$sparam, ')'), 
+                   ifelse (!is.na(args_table$sparam), paste0(' (', args_table$sparam, ')'), 
                            paste(rep(' ', 5), collapse = '')),
                    ifelse (args_table$desc == '', '', ': '), 
                    args_table$desc, 
@@ -429,9 +429,8 @@ test_parser <- function() {
   reg_argument("--infile","-i","infile",NA,argsType$TypeValue,'select infile')
   reg_argument("--outfile","-o","outfile",NA,argsType$TypeValue,'specify outfile')
   reg_argument("--date","-d","date",NA,argsType$TypeValue,'specify date')
-  # an example lparam w/ no sparam & vis versa
+  # an example lparam w/ no sparam
   reg_argument("--noshort",NA,"noshort",FALSE,argsType$TypeBool, "no short form argument")
-  reg_argument('','-n',"nolong",FALSE,argsType$TypeBool, "no long form argument")
   # an example TypeMultiVal, where all supplied params are stored
   reg_argument("--keyword","-k","keyword",NA,argsType$TypeMultiVal,'keywords')
   #  test to ensure --ver/-V not added if a conflicting param is already registered
@@ -467,7 +466,6 @@ test_parser <- function() {
   writeLines (paste("command:",mydata$command))
   writeLines (paste("subcommand:",mydata$subcmd))
   writeLines (paste("noshort:",mydata$noshort))
-  writeLines (paste("nolong:",mydata$nolong))
   writeLines (paste("keyword:",mydata$keyword))
 
   writeLines ("\nParsing dates...")
