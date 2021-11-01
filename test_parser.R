1d4e5e0ec7bc59b2924c92b4784ba7c8cf46b98e
Jeffrey Perkel <j_perkel@yahoo.com>
2021-10-31 21:27:44 -0600

    Remove 'scope' arguments from test_parser.R, as they are deprecated. Clean up example code.

--- test_parser.R
+++ test_parser.R
@@ -6,7 +6,7 @@ init_command_line_parser('MyCheckbook.R','My checkbook program', '1.0.0')
 
 # we can register arguments one at a time, eg:  
 # an example TypeBool; default == FALSE; if used in cmdline, will be set to TRUE
-reg_argument("--sort-rev-chronological",NA,"sort_rev_chronological",FALSE,argsType$TypeBool,'Display newest entries first')
+reg_argument("--search-or",NA,"search_or",FALSE,argsType$TypeBool,'Any keyword will match (Boolean OR)')
 # example TypeValue arguments. Use as '--lparam=val', '--lparam val', or '-l val'
 reg_argument("--infile","-i","infile",NA,argsType$TypeValue,'location of your checkbook file',"command|subcmd")
 
@@ -16,11 +16,11 @@ arguments <- list(
   list("--outfile","-o","outfile",NA,argsType$TypeValue,'location of output file'),
   list("--date","-d","date",NA,argsType$TypeValue,'specify date'),
   # an example argument whose scope is limited to command = withdraw (with any subcmd)
-  list("--msg","-m","msg",NA,argsType$TypeValue,'memo line message',"withdraw"),
+  list("--msg","-m","msg",NA,argsType$TypeValue,'memo line message'),
   list("--amount","-a","amount",NA,argsType$TypeValue,'specify dollar amount'),
   # example arguments whose scope is limited to command = withdraw, subcmd = check
-  list("--payee","-p","payee",NA,argsType$TypeValue,'specify payee',c("withdraw|check","deposit")),
-  list("--number","-n","cknum",NA,argsType$TypeValue,'specify check number',"withdraw|check"),
+  list("--payee","-p","payee",NA,argsType$TypeValue,'specify payee'),
+  list("--number","-n","cknum",NA,argsType$TypeValue,'specify check number'),
   # an example TypeMultiVal, where all supplied params are stored
   list("--keyword","-k","keyword",NA,argsType$TypeMultiVal,'keywords'),
   # an example TypeCount, where each use of the param increments a variable
@@ -40,7 +40,7 @@ reg_argument_list(arguments)
 # or as a list
 cmds <- list(
   list("withdraw", "add a withdrawal"),
-  list("plot", "graph output"),
+#  list("plot", "graph output"),
   list("deposit", "add a deposit"),
   list("edit", "update a record"),
   list("find", "find a record")
@@ -52,7 +52,7 @@ reg_subcmd("cash", "withdraw", "add a cash withdrawal")
 reg_subcmd("check", "withdraw", "add a check withdrawal")
 
 subcmds <- list(
-  list("paycheck", "deposit", "add a paycheck deposit"),
+  # list("paycheck", "deposit", "add a paycheck deposit"),
   list("reimbursement", "deposit", "add a reimbursement"),
   list("bankfee", "withdraw", "add a bank fee"),
   list("check", "deposit", "add a check deposit")
@@ -69,7 +69,7 @@ writeLines ("Done!")
 writeLines ("\nAfter parse_command_line()...")
 writeLines (paste("command:",mydata$command))
 writeLines (paste("subcommand:",mydata$subcmd))
-writeLines (paste("sort_rev_chronological:", mydata$sort_rev_chronological))
+writeLines (paste("search-or:", mydata$search_or))
 writeLines (paste("infile:", mydata$infile))
 writeLines (paste("outfile:",mydata$outfile))
 writeLines (paste("date:",mydata$date))
@@ -80,7 +80,7 @@ writeLines (paste("cknum:",mydata$cknum))
 writeLines (paste("keywords:",mydata$keyword))
 writeLines (paste("unknowns:",mydata$unknowns))
 writeLines (paste("verbose level:", mydata$verbose))
-writeLines (paste("range:", mydata$range1, '-', mydata$range2))
+writeLines (paste("range:", ifelse(is.na(mydata$range), NA, paste(mydata$range1, '-', mydata$range2))))
 # writeLines (paste("file 1:", mydata$file1))
 # writeLines (paste("file 2:", mydata$file2))
 # writeLines (paste("file 3:", mydata$file3))
